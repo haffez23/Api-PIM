4e158de7154c33591049958e0b7606491630f508
HAFFEZ <haffez23@gmail.com>
2019-04-30 12:21:30 +0100

    Convert Text To image to Hex C String

--- controller/image.js
+++ controller/image.js
@@ -1,16 +1,68 @@
 
 var base64Img = require('base64-img');
+var Jimp = require('jimp');
+var fs = require('fs');
+var jpeg = require('jpeg-js');
+const converter = require('image_to_epaper_converter');
 // Handle index actions
 exports.index = function (req, res) {
-    var webshot = require('webshot');
+  var webshot = require('webshot');
+  var web3 = require('web3');
+  var options = {
+    screenSize: {
+      width: 128
+    , height: 296
+    }
+  , shotSize: {
+      width: 128
+    , height: 296
+    }
+  , userAgent: 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_2 like Mac OS X; en-us)'
+      + ' AppleWebKit/531.21.20 (KHTML, like Gecko) Mobile/7B298g'
+  
+  ,
+  siteType:'html'
+  }
+  ;
 
-    webshot('<html><body>Hello World</body></html>', 'images/messages/hello_world.png', {siteType:'html'}, function(err) {
+    webshot('<html ><body style="background-color: #ffffff;">Hello World</body></html>', 'images/messages/hello_world.png', options, function(err) {
       // screenshot now saved to hello_world.png
-      base64Img.base64('images/messages/hello_world.png', function(err, data) {
 
+      var width = 298, height = 128;
 
-        res.send(data);
+      Jimp.read("images/messages/hello_world.png", function (err, image) {
+     
         
+        converter.convert({
+          source_file: "images/messages/hello_world.png",
+          target_folder: "images/active",
+          target_text_filename: "picture.txt",
+          target_cpp_filename: "picture",
+          cpp_variable_name: "PICTURE",
+          tasks: [ "binary", "hexadecimal", "hexadecimal_cpp" ],
+          display: {
+              "width": 128,
+              "height":  296,
+              "fitmode": "center",
+              "colormode": "inverted"
+          }
+      });
+
+      var data = fs.readFileSync('images/active/ext.txt', 'utf8');  
+          res.send(data)
+
+       // console.log(web3.toHex(rawImageData) );
+
       })
     });
+
+
+
+
+
 };
+
+
+
+
+
